85701cf959324586eb493d31e636c14e7f22e279
sreevyshnavi02 <sreevysh2002@gmail.com>
2023-04-14 13:33:23 +0530

    modified connection for efficiency and security

--- connection.php
+++ connection.php
@@ -1,13 +1,22 @@
 <?php
     session_start();
-    $dsn = 'mysql:host=localhost;dbname=sel_project';
-    $username = 'root';
-    $password = '';
+    // Establish database connection
+    $host = "localhost";
+    $dbname = "sel_project";
+    $username = "root";
+    $password = "";
+    $charset = "utf8mb4";
 
+    $dsn = "mysql:host=$host;dbname=$dbname;charset=$charset";
+    $options = [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES => false,
+    ];
     try {
-        $conn = new PDO($dsn, $username, $password);
-    } catch (PDOException $e) {
-        echo 'Connection failed: ' . $e->getMessage();
+        $conn = new PDO($dsn, $username, $password, $options);
+    } catch (\PDOException $e) {
+        throw new \PDOException($e->getMessage(), (int)$e->getCode());
     }
 
 ?>
