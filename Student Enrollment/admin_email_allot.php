b847528abb1b51bd46db886847909389eabc926a
sreevyshnavi02 <sreevysh2002@gmail.com>
2023-04-16 13:59:28 +0530

    modified to generate unique mail for same sname

--- Student Enrollment/admin_email_allot.php
+++ Student Enrollment/admin_email_allot.php
@@ -3,12 +3,13 @@
 include '../header.php';
 // Fetch students admitted in a particular semester
 $yoj = $_POST['yoj'];
-$stmt = $conn->prepare("SELECT REGNO, SNAME FROM u_student WHERE YOJ=:yoj");
+$stmt = $conn->prepare("SELECT REGNO, SNAME FROM u_student WHERE YOJ=:yoj and email is NULL");
 $stmt->execute(['yoj' => $yoj]);
 $students = $stmt->fetchAll();
 
 // Generate email ids for each student
 foreach ($students as $student) {
+    echo $student['SNAME'];
     $name_parts = explode(" ", $student['SNAME']);
     $prefix = "";
     foreach ($name_parts as $part) {
@@ -23,17 +24,21 @@ foreach ($students as $student) {
     // Check if email id already exists
     $suffix = "";
     $extension = "@ptuniv.edu.in";
-    $email = $prefix . $extension;
     $stmt = $conn->prepare("SELECT REGNO FROM u_student WHERE EMAIL=:email");
     do {
         $stmt->execute(['email' => $prefix.$suffix.$extension]);
-        $result = $stmt->fetch();
+        $result = $stmt->rowCount();
+        echo "result = ".$result." when email = ".$prefix.$suffix.$extension."<br>";
         $suffix++;
+        echo "suffix = ".$suffix."<br>";
     } while ($result);
+
+    $suffix --;
 
     // Update student's email id in the database
     $stmt = $conn->prepare("UPDATE u_student SET EMAIL=:email WHERE REGNO=:regno");
-    $stmt->execute(['email' => $email . $suffix, 'regno' => $student['REGNO']]);
+    $email = $prefix.$suffix.$extension;
+    $stmt->execute(['email' => $email, 'regno' => $student['REGNO']]);
     echo "updated the email ".$email."<br>";
 }
 
