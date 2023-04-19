6929d72fb010a300a7e033bf95f8fb06668366e7
sreevyshnavi02 <sreevysh2002@gmail.com>
2023-04-19 19:40:54 +0530

    added 0 cred, hm course, stud details

--- course_regn/course_regn.php
+++ course_regn/course_regn.php
@@ -10,11 +10,16 @@
 
     <?php 
         include "../header.php"; 
-        $stud_data = get_stud_data($conn, $_SESSION['regno']);   
+        $stud_data = get_stud_data($conn, $_SESSION['regno']); 
+        echo "<div class='stud_details_crs'>";
+        echo "<h3> Regno: ".$stud_data['regno']."</h3>";  
+        echo "<h3> Name: ".$stud_data['sname']."</h3>";  
+        echo "<h3> Sem: ".$stud_data['curr_sem']."</h3>";  
+        echo "</div>";
     ?>
-
+    <div class="course_regn">
     <!-- Display compulsory courses -->
-    <h2>Compulsory Courses</h2>
+    <h2 style='text-align:center; padding: 1rem;'>Compulsory Courses</h2>
     <table class="courses">
         <tr>
             <th>Course Code</th>
@@ -46,7 +51,7 @@
     $oec = fetch_oec($conn, $stud_data['regno'], $_SESSION['session']);
     if($oec != 0){
     ?>
-        <h2>Open Elective Course</h2>
+        <h2 style='text-align:center; padding: 1rem;'>Open Elective Course</h2>
         <table class="courses">
             <tr>
                 <th>Course Code</th>
@@ -74,12 +79,16 @@
         ?>
 
     <!-- Display Program elective courses offered for that sem to choose from -->
+
+
+    <!-- Display Honor/Minor course if applicable -->
+
     <?php
     $_SESSION['session'] = '23A';
-    $oec = fetch_hm($conn, $stud_data['regno'], $_SESSION['session']);
+    $hm_course = fetch_hm($conn, $stud_data['regno'], $stud_data['curr_sem']);
     if($hm_course != 0){
     ?>
-        <h2>Honor/Minor Course</h2>
+        <h2 style='text-align:center; padding: 1rem;'>Honor/Minor Course</h2>
         <table class="courses">
             <tr>
                 <th>Course Code</th>
@@ -106,9 +115,52 @@
     }
     ?>
 
-    <!-- Display Honor/Minor course if applicable -->
 
     <!-- Display uncleared mandatory 0 cred course if not cleared in the prev sem -->
+    
+    <?php
+    $_SESSION['session'] = '23A';
+    $hm_course = fetch_uncleared_mandatory($conn, $stud_data['regno'], $_SESSION['session']);
+    if($hm_course != 0){
+    ?>
+        <h2 style='text-align:center; padding: 1rem;'>Mandatory 0 credit Course(uncleared)</h2>
+        <table class="courses">
+            <tr>
+                <th>Course Code</th>
+                <th>Course Name</th>
+                <th>Credits</th>
+                <th>Faculty 1</th>
+                <th>Faculty 2</th>
+            </tr>
+            
+            <?php
+            foreach($oec as $c){
+                $course = fetch_course_details($conn, $c['course_code']);
+                echo "<tr>";
+                echo "<td>".$course['COURSE_CODE']."</td>";
+                echo "<td>".$course['COURSE_NAME']."</td>";
+                echo "<td>".$course['CREDITS']."</td>";
+                display_fac_names($conn, 'comp1');
+                display_fac_names($conn, 'comp2');
+                echo "</tr>";
+            }
+            ?>
+        </table>
+    <?php
+    }
+    ?>
+        </div>
+
+        <button class="small_btn" onclick="get_confirmation()">Submit</button>
+
+        <script>
+            function get_confirmation() {
+                alert("Make sure you have checked the details. Are you sure to submit?");
+                window.location.href = "submit_course_regn.php";
+            }
+        </script>
+
+
 </body>
 </html>
 
@@ -138,7 +190,6 @@
         where regno = '$regno' and session = '$session'");
 
         $query -> execute();
-
         $n = $query -> rowCount();
 
         if($n == 0){
@@ -149,6 +200,57 @@
             return $result;
         }
     }
+
+
+    function fetch_hm($conn, $regno, $curr_sem){
+        $query = $conn -> query("select alloted_prgm 
+        from u_hm_allotment 
+        where regno = '$regno' and withdrawal_date is null");
+
+        $query -> execute();
+        $n = $query -> rowCount();
+        
+        if($n == 0){
+            // not in any hm prgm
+            return 0;
+        }
+        else{
+            $result = $query -> fetch(PDO::FETCH_ASSOC);
+            $prgm_id = $result['alloted_prgm'];
+
+            $query2 = $conn -> query("select course_code 
+            from u_prgm_comp_course 
+            where prgm_id = '$prgm_id' and sem = '$curr_sem'");
+
+            $query2 -> execute();
+
+            $result2 = $query2 -> fetchAll(PDO::FETCH_ASSOC);
+            return $result2;
+        }
+    }
+
+    function fetch_uncleared_mandatory($conn, $regno, $session){
+        $month = $session[2];
+        // echo "month = ".$month."<br>";
+        $query = $conn -> query("select e.course_code 
+        from u_external_marks e, u_course c
+        where e.regno = '$regno' and 
+        e.grade like 'F' and 
+        c.course_code = e.course_code and 
+        c.credits = 0 and e.session like '%%$month'");
+
+        $query -> execute();
+        $n = $query -> rowCount();
+
+        if($n == 0){
+            return 0;
+        }
+        else{
+            $result = $query -> fetchAll(PDO::FETCH_ASSOC);
+            return $result;
+        }
+    }
+
 
     function fetch_course_details($conn, $course_code){
         $query = $conn -> query("select * 
