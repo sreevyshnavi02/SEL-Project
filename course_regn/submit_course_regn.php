9d4d89eaa99201e73e0406c3fe811c066b1a3edb
sreevyshnavi02 <sreevysh2002@gmail.com>
2023-04-19 19:41:39 +0530

    confirmation page after course regn

---
+++ course_regn/submit_course_regn.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PTU-COE</title>
+</head>
+<body>
+    <?php include '../header.php'; ?>
+
+    <h2 style="text-align: center">You have successfully registered for the courses! All the best!</h2>
+</body>
+</html>
