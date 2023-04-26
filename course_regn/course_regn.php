4f6b5ebc6adc3048084a1f7e7f39d80d2c4116de
sreevyshnavi02 <sreevysh2002@gmail.com>
2023-04-26 12:08:01 +0530

    check if course regn is already done

--- course_regn/course_regn.php
+++ course_regn/course_regn.php
@@ -16,147 +16,187 @@
         echo "<h3> Name: ".$stud_data['sname']."</h3>";  
         echo "<h3> Sem: ".$stud_data['curr_sem']."</h3>";  
         echo "</div>";
-    ?>
-    <div class="course_regn">
-    <!-- Display compulsory courses -->
-    <h2 style='text-align:center; padding: 1rem;'>Compulsory Courses</h2>
-    <table class="courses">
-        <tr>
-            <th>Course Code</th>
-            <th>Course Name</th>
-            <th>Credits</th>
-            <th>Faculty 1</th>
-            <th>Faculty 2</th>
-        </tr>
-        <?php
-        $comp_courses = fetch_comp_courses($conn, $stud_data['prgm_id'], $stud_data['curr_sem']);
 
-        foreach($comp_courses as $c){
-            $course = fetch_course_details($conn, $c['course_code']);
-            echo "<tr>";
-            echo "<td>".$course['COURSE_CODE']."</td>";
-            echo "<td>".$course['COURSE_NAME']."</td>";
-            echo "<td>".$course['CREDITS']."</td>";
-            display_fac_names($conn, 'comp1');
-            display_fac_names($conn, 'comp2');
-            echo "</tr>";
+        $_SESSION['curr_sem'] = $stud_data['curr_sem'];
+        
+        // Get the regno and curr_sem values from the $stud_data array
+        $regno = $stud_data['regno'];
+        $curr_sem = $stud_data['curr_sem'];
+
+        // Prepare a PDO statement to check if the values exist in the table
+        $stmt = $conn->prepare("SELECT * FROM u_course_regn WHERE regno = :regno AND sem = :curr_sem");
+
+        // Bind the values to the prepared statement
+        $stmt->bindParam(':regno', $regno);
+        $stmt->bindParam(':curr_sem', $curr_sem);
+
+        // Execute the prepared statement
+        $stmt->execute();
+
+        // Check if the number of rows returned is greater than 0
+        if ($stmt->rowCount() > 0) {
+            // The values exist in the table
+            echo "<h1>Course registration already done!</h1>";
+
+            //redirect to course_regn_confirmation.php
+            // header("Location: course_regn_confirmation.php");
+        } else {
+            // The values do not exist in the table
+            //creating a 2d array that can be used for pushing the courses regd for
+            $row_count = 0;
+        ?>
+        <form class="course_regn" action="submit_course_regn.php" method="post">
+
+        <!-- Display compulsory courses -->
+        <h2 style='text-align:center; padding: 1rem; margin-bottom: 1rem;'>Compulsory Courses</h2>
+        <table class="courses">
+            <tr>
+                <th>Course Code</th>
+                <th>Course Name</th>
+                <th>Credits</th>
+                <th>Faculty 1</th>
+                <th>Faculty 2</th>
+            </tr>
+            <?php
+            $comp_courses = fetch_comp_courses($conn, $stud_data['prgm_id'], $stud_data['curr_sem']);
+
+            foreach($comp_courses as $c){
+                $course = fetch_course_details($conn, $c['course_code']);
+                echo "<tr>";
+                echo '<td><input type="text" value = "'.$course["COURSE_CODE"].'" name = "data['.$row_count.'][]"></td>';
+                // echo '<td><input type="text" value = "'.$course["COURSE_CODE"].'" name = "course_code[]" ></td>';
+                echo "<td>".$course['COURSE_NAME']."</td>";
+                echo "<td>".$course['CREDITS']."</td>";
+                display_fac_names($conn, $row_count);
+                display_fac_names($conn, $row_count);
+                echo "</tr>";
+                $row_count ++;
+            }
+            ?>
+        </table>
+
+
+        <!-- Display Open elective courses if applicable -->
+        <?php
+        $_SESSION['session'] = '23A';
+        $oec = fetch_oec($conn, $stud_data['regno'], $_SESSION['session']);
+        if($oec != 0){
+        ?>
+            <h2 style='text-align:center; padding: 1rem; margin-bottom: 1rem;'>Open Elective Course</h2>
+            <table class="courses">
+                <tr>
+                    <th>Course Code</th>
+                    <th>Course Name</th>
+                    <th>Credits</th>
+                    <th>Faculty 1</th>
+                    <th>Faculty 2</th>
+                </tr>
+                
+                <?php
+                foreach($oec as $c){
+                    $course = fetch_course_details($conn, $c['course_code']);
+                    echo "<tr>";
+                    echo "<td><input type='text' value = '".$course['COURSE_CODE']."' readonly name='data[".$row_count."][]'></td>";
+                    echo "<td>".$course['COURSE_NAME']."</td>";
+                    echo "<td>".$course['CREDITS']."</td>";
+                    display_fac_names($conn, $row_count);
+                    display_fac_names($conn, $row_count);
+                    echo "</tr>";
+                    $row_count ++;
+                    
+                }
+                ?>
+            </table>
+            <?php
+            }
+            ?>
+
+        <!-- Display Program elective courses offered for that sem to choose from -->
+
+
+        <!-- Display Honor/Minor course if applicable -->
+
+        <?php
+        $_SESSION['session'] = '23A';
+        $hm_course = fetch_hm($conn, $stud_data['regno'], $stud_data['curr_sem']);
+        if($hm_course != 0){
+        ?>
+            <h2 style='text-align:center; padding: 1rem; margin-bottom: 1rem;'>Honor/Minor Course</h2>
+            <table class="courses">
+                <tr>
+                    <th>Course Code</th>
+                    <th>Course Name</th>
+                    <th>Credits</th>
+                    <th>Faculty 1</th>
+                    <th>Faculty 2</th>
+                </tr>
+                
+                <?php
+                foreach($hm_course as $c){
+                    $course = fetch_course_details($conn, $c['course_code']);
+                    echo "<tr>";
+                    echo "<td><input type='text' value = '".$course['COURSE_CODE']."' readonly name='data[".$row_count."][]'></td>";
+                    echo "<td>".$course['COURSE_NAME']."</td>";
+                    echo "<td>".$course['CREDITS']."</td>";
+                    display_fac_names($conn, $row_count);
+                    display_fac_names($conn, $row_count);
+                    echo "</tr>";
+                    $row_count ++;
+
+                }
+                ?>
+            </table>
+        <?php
         }
         ?>
-    </table>
 
 
-    <!-- Display Open elective courses if applicable -->
-    <?php
-    $_SESSION['session'] = '23A';
-    $oec = fetch_oec($conn, $stud_data['regno'], $_SESSION['session']);
-    if($oec != 0){
-    ?>
-        <h2 style='text-align:center; padding: 1rem;'>Open Elective Course</h2>
-        <table class="courses">
-            <tr>
-                <th>Course Code</th>
-                <th>Course Name</th>
-                <th>Credits</th>
-                <th>Faculty 1</th>
-                <th>Faculty 2</th>
-            </tr>
-            
-            <?php
-            foreach($oec as $c){
-                $course = fetch_course_details($conn, $c['course_code']);
-                echo "<tr>";
-                echo "<td>".$course['COURSE_CODE']."</td>";
-                echo "<td>".$course['COURSE_NAME']."</td>";
-                echo "<td>".$course['CREDITS']."</td>";
-                display_fac_names($conn, 'comp1');
-                display_fac_names($conn, 'comp2');
-                echo "</tr>";
-            }
-            ?>
-        </table>
+        <!-- Display uncleared mandatory 0 cred course if not cleared in the prev sem -->
+        
+        <?php
+        $_SESSION['session'] = '23A';
+        $zero_cred = fetch_uncleared_mandatory($conn, $stud_data['regno'], $_SESSION['session']);
+        if($zero_cred != 0){
+        ?>
+            <h2 style='text-align:center; padding: 1rem; margin-bottom: 1rem;'>Mandatory 0 credit Course(uncleared)</h2>
+            <table class="courses">
+                <tr>
+                    <th>Course Code</th>
+                    <th>Course Name</th>
+                    <th>Credits</th>
+                    <th>Faculty 1</th>
+                    <th>Faculty 2</th>
+                </tr>
+                
+                <?php
+                foreach($zero_cred as $c){
+                    $course = fetch_course_details($conn, $c['course_code']);
+                    echo "<tr>";
+                    echo "<td><input type='text' value = '".$course['COURSE_CODE']."' readonly name='data[".$row_count."][]'></td>";
+                    echo "<td>".$course['COURSE_NAME']."</td>";
+                    echo "<td>".$course['CREDITS']."</td>";
+                    display_fac_names($conn, $row_count);
+                    display_fac_names($conn, $row_count);
+                    echo "</tr>";
+                    $row_count ++;
+                }
+                ?>
+            </table>
+        <?php
+        }
+        ?>
+        <button type="submit" class="small_btn" onclick="get_confirmation()">Submit</button>
+        </form>
         <?php
         }
         ?>
 
-    <!-- Display Program elective courses offered for that sem to choose from -->
 
-
-    <!-- Display Honor/Minor course if applicable -->
-
-    <?php
-    $_SESSION['session'] = '23A';
-    $hm_course = fetch_hm($conn, $stud_data['regno'], $stud_data['curr_sem']);
-    if($hm_course != 0){
-    ?>
-        <h2 style='text-align:center; padding: 1rem;'>Honor/Minor Course</h2>
-        <table class="courses">
-            <tr>
-                <th>Course Code</th>
-                <th>Course Name</th>
-                <th>Credits</th>
-                <th>Faculty 1</th>
-                <th>Faculty 2</th>
-            </tr>
-            
-            <?php
-            foreach($oec as $c){
-                $course = fetch_course_details($conn, $c['course_code']);
-                echo "<tr>";
-                echo "<td>".$course['COURSE_CODE']."</td>";
-                echo "<td>".$course['COURSE_NAME']."</td>";
-                echo "<td>".$course['CREDITS']."</td>";
-                display_fac_names($conn, 'comp1');
-                display_fac_names($conn, 'comp2');
-                echo "</tr>";
-            }
-            ?>
-        </table>
-    <?php
-    }
-    ?>
-
-
-    <!-- Display uncleared mandatory 0 cred course if not cleared in the prev sem -->
-    
-    <?php
-    $_SESSION['session'] = '23A';
-    $hm_course = fetch_uncleared_mandatory($conn, $stud_data['regno'], $_SESSION['session']);
-    if($hm_course != 0){
-    ?>
-        <h2 style='text-align:center; padding: 1rem;'>Mandatory 0 credit Course(uncleared)</h2>
-        <table class="courses">
-            <tr>
-                <th>Course Code</th>
-                <th>Course Name</th>
-                <th>Credits</th>
-                <th>Faculty 1</th>
-                <th>Faculty 2</th>
-            </tr>
-            
-            <?php
-            foreach($oec as $c){
-                $course = fetch_course_details($conn, $c['course_code']);
-                echo "<tr>";
-                echo "<td>".$course['COURSE_CODE']."</td>";
-                echo "<td>".$course['COURSE_NAME']."</td>";
-                echo "<td>".$course['CREDITS']."</td>";
-                display_fac_names($conn, 'comp1');
-                display_fac_names($conn, 'comp2');
-                echo "</tr>";
-            }
-            ?>
-        </table>
-    <?php
-    }
-    ?>
-        </div>
-
-        <button class="small_btn" onclick="get_confirmation()">Submit</button>
 
         <script>
             function get_confirmation() {
                 alert("Make sure you have checked the details. Are you sure to submit?");
-                window.location.href = "submit_course_regn.php";
+                // window.location.href = "submit_course_regn.php";
             }
         </script>
 
@@ -200,7 +240,6 @@
             return $result;
         }
     }
-
 
     function fetch_hm($conn, $regno, $curr_sem){
         $query = $conn -> query("select alloted_prgm 
@@ -251,7 +290,6 @@
         }
     }
 
-
     function fetch_course_details($conn, $course_code){
         $query = $conn -> query("select * 
         from u_course 
@@ -261,11 +299,11 @@
         return $row;
     }
 
-    function display_fac_names($conn, $fac_for){
+    function display_fac_names($conn, $row_count){
         $query = $conn -> query("select faculty_id, fname from u_faculty");
         $query -> execute();
         $fac_names = $query -> fetchAll(PDO::FETCH_ASSOC);
-        echo "<td><select name='faculty".$fac_for."[]'>";
+        echo "<td><select name='data[".$row_count."][]'>";
         print_r($fac_names);
         echo "<option value=''>Choose Faculty</option>";
         foreach($fac_names as $fac){
