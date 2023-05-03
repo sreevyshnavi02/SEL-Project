deefc1c4bd6e98b30d8704b69cb506b002e7cdf7
sreevyshnavi02 <sreevysh2002@gmail.com>
2023-05-03 05:58:48 +0530

    btn to manage regns

--- admin.php
+++ admin.php
@@ -8,13 +8,13 @@
     <body>
         <?php include 'header.php'; ?>
         <div class="provision">
-            <a href = "./enable_disable_regns.php"><button class = 'btnn'>Manage student registrations</button></a>
+            <a href = "./enable_disable_regns_session.php"><button class = 'btnn'>Manage student registrations</button></a>
             <a href = "./Student Enrollment/admin_before_regno_gen.php"><button class = 'btnn'>Generate Register Numbers</button></a>
             <a href = "./Student Enrollment/admin_before_email_gen.php"><button class = 'btnn'>Generate Institution Email IDs</button></a>
             <!-- <a href = "./Student Enrollment/nominal_roll.php"><button class = 'btnn'>Generate Nominal Roll</button></a> -->
             <a href = "./OEC_allotment/admin_get_session.php"><button class = 'btnn'>Enable OEC Allotment</button></a>
             <a href = "./Honor_minor_allotment/admin_hm_allotment.php"><button class = 'btnn'>Enable Honor/Minor Allotment</button></a>
-            <a href = #><button class = 'btnn'>Enable Course Registration</button></a>
+            <!-- <a href = #><button class = 'btnn'>Enable Course Registration</button></a> -->
             <a href = exam_regn/admin_get_session.php><button class = 'btnn'>Enable Exam Registration</button></a>
         </div>
 
