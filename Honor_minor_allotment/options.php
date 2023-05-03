dda29e33c0cc2f9f627623c23635e298b560b0a7
sreevyshnavi02 <sreevysh2002@gmail.com>
2023-05-03 05:54:11 +0530

    indented the text

--- Honor_minor_allotment/options.php
+++ Honor_minor_allotment/options.php
@@ -29,45 +29,43 @@
         $stud_details = $stud_details_query -> fetch(PDO::FETCH_ASSOC);
         $stud_exist = $stud_details_query -> rowCount();
         if($stud_exist){
+            
+            $_SESSION['dept_id'] = $stud_details['dept_id'];
+            $_SESSION['cgpa'] = $stud_details['cgpa'];
 
-        $_SESSION['dept_id'] = $stud_details['dept_id'];
-        $_SESSION['cgpa'] = $stud_details['cgpa'];
+            $check_prereg = $conn -> query ("
+            select * from u_hm_preregistration
+            where regno = '$regno' ");
 
-        $check_prereg = $conn -> query ("
-        select * from u_hm_preregistration
-        where regno = '$regno' ");
+            $n = $check_prereg -> rowCount();
 
-        $n = $check_prereg -> rowCount();
+            //displaying the cgpa
+            if($n > 0){
+                echo "<h1 style='text-align: center; background-color: gray; padding: 1rem'>You have already given the options. </h1><br>";   
+            ?>
+                <button class="small_btn" onclick="goback()">Back</button>
+                <script>
+                    function goback() {
+                        window.location.href = "../student_login.php";
+                    }
+                </script> 
+            <?php
+            }
+            elseif(($stud_details['curr_sem'] == 3 && $stud_details['entry_mode'] == 'R' && $stud_details['cgpa'] > 7.5) ||
+            ($stud_details['curr_sem'] == 4 && $stud_details['entry_mode'] == 'L' && $stud_details['cgpa'] > 7.5)){
+                echo "<h1 style='text-align: center; background-color: gray; padding: 1rem'>Your CGPA: ".$stud_details['cgpa']."</h1><br>";
+            ?>
+            <h1 style="text-align: center">Honor/Minor Pre-registration</h1>
+            <form method="POST" action="save_options.php">
+            <?php
+                $fetch_stud_details_sql = "select sname, curr_sem, credits_earned from u_student where regno = :regno";
+                $fetch_stud_details = $conn -> prepare($fetch_stud_details_sql);
+                $fetch_stud_details -> bindParam(':regno', $_SESSION['regno']);
+                $fetch_stud_details -> execute();
+                $n = $fetch_stud_details -> rowCount();
 
-        //displaying the cgpa
-        if($n > 0){
-            echo "<h1 style='text-align: center; background-color: gray; padding: 1rem'>You have already given the options. </h1><br>";   
-        ?>
-            <button class="small_btn" onclick="goback()">Back</button>
-
-            <script>
-                function goback() {
-                    window.location.href = "../student_login.php";
-                }
-            </script> 
-        <?php
-        }
-        elseif(($stud_details['curr_sem'] == 3 && $stud_details['entry_mode'] == 'R' && $stud_details['cgpa'] > 7.5) ||
-        ($stud_details['curr_sem'] == 4 && $stud_details['entry_mode'] == 'L' && $stud_details['cgpa'] > 7.5)){
-            echo "<h1 style='text-align: center; background-color: gray; padding: 1rem'>Your CGPA: ".$stud_details['cgpa']."</h1><br>";
-    ?>
-	<h1 style="text-align: center">Honor/Minor Pre-registration</h1>
-	<form method="POST" action="save_options.php">
-    <?php
-        $fetch_stud_details_sql = "select sname, curr_sem, credits_earned from u_student where regno = :regno";
-        $fetch_stud_details = $conn -> prepare($fetch_stud_details_sql);
-        $fetch_stud_details -> bindParam(':regno', $_SESSION['regno']);
-        $fetch_stud_details -> execute();
-        $n = $fetch_stud_details -> rowCount();
-
-        $fetched_stud_data = $fetch_stud_details -> fetchAll(PDO::FETCH_ASSOC);
-
-    ?>
+                $fetched_stud_data = $fetch_stud_details -> fetchAll(PDO::FETCH_ASSOC);
+            ?>
 
             <!-- display student details -->
             <table class="left-align-table">
@@ -86,96 +84,81 @@
                 <?php
             }?>
 
-        <tr>
-            <?php
-				// Retrieve the list of courses from the database using PDO
-                $dept_id = $_SESSION['dept_id'];
-                $hm_prgms_query = "SELECT * FROM u_prgm WHERE (LOWER(prgm_name) LIKE '%honors%' AND dept_id = '$dept_id') OR (LOWER(prgm_name) LIKE '%minors%' AND dept_id != '$dept_id') AND offered = 1";
-				$hm_prgms_prepare = $conn->prepare($hm_prgms_query);
-                // $hm_prgms_prepare -> bindParam(':dept_id', $dept_id);
-                
-                $hm_prgms_prepare -> execute();
-                
-                $hm_prgms = $hm_prgms_prepare -> fetchAll(PDO::FETCH_ASSOC);
-            ?>
-            <td><label for="preference1">Preference 1:</label></td>
-            <td><select name="preference1" id="preference1" required>
-            <option value="">Select a Program</option>
-			<?php
-				// Generate the options for the dropdown list
-				foreach($hm_prgms as $row){
-                    echo "<script>console.log('inside while')</script>";
-                    echo "<script>console.log('".$row['PRGM_ID']."')</script>";
-					echo '<option value="' . $row['PRGM_ID'] . '">' . $row['DEPT_ID'] .' - ' .$row['PRGM_NAME'] .'</option>';
-				}
-                
+            <tr>
+                <?php
+                    // Retrieve the list of courses from the database using PDO
+                    $dept_id = $_SESSION['dept_id'];
+                    $hm_prgms_query = "SELECT * FROM u_prgm WHERE (LOWER(prgm_name) LIKE '%honors%' AND dept_id = '$dept_id') OR (LOWER(prgm_name) LIKE '%minors%' AND dept_id != '$dept_id') AND offered = 1";
+                    $hm_prgms_prepare = $conn->prepare($hm_prgms_query);
+                    // $hm_prgms_prepare -> bindParam(':dept_id', $dept_id);
+                    
+                    $hm_prgms_prepare -> execute();
+                    
+                    $hm_prgms = $hm_prgms_prepare -> fetchAll(PDO::FETCH_ASSOC);
                 ?>
-		</select>
-        </td>
-        </tr>
-
-        <tr>
-
-            <td><label for="preference2">Preference 2:</label></td>
-            <td><select name="preference2" id="preference2" required>
+                <td><label for="preference1">Preference 1:</label></td>
+                <td><select name="preference1" id="preference1" required>
                 <option value="">Select a Program</option>
-			<?php
-
-                // Retrieve the list of courses from the database using PDO
-                $courses_query = "SELECT * FROM u_prgm WHERE (LOWER(prgm_name) LIKE '%honors%' AND dept_id = '$_SESSION[dept_id]') OR (LOWER(prgm_name) LIKE '%minors%' AND dept_id != '$_SESSION[dept_id]') AND offered = 1";
-                $courses_result = $conn->query($courses_query);
-
-                // Generate the options for the dropdown list
-                while ($row = $courses_result->fetch(PDO::FETCH_ASSOC)) {
-					echo '<option value="' . $row['PRGM_ID'] . '">' . $row['DEPT_ID'] .' - ' .$row['PRGM_NAME'] .'</option>';
-                }
-                
-                ?>
-		</select>
-        </td>
-    </tr>
-    <tr>
-        <td><label for="preference3">Preference 3:</label></td>
-		<td>
-            <select name="preference3" id="preference3" required>
-            <option value="">Select a Program</option>
-			<?php
-
-                // Retrieve the list of courses from the database using PDO
-                $courses_query = "SELECT * FROM u_prgm WHERE (LOWER(prgm_name) LIKE '%honors%' AND dept_id = '$_SESSION[dept_id]') OR (LOWER(prgm_name) LIKE '%minors%' AND dept_id != '$_SESSION[dept_id]') AND offered = 1";
-                $courses_result = $conn->query($courses_query);
-
-                // Generate the options for the dropdown list
-                while ($row = $courses_result->fetch(PDO::FETCH_ASSOC)) {
-                    echo '<option value="' . $row['PRGM_ID'] . '">' . $row['DEPT_ID'] .' - ' .$row['PRGM_NAME'] .'</option>';
-                }
-            }
-
-            ?>
+                <?php
+                    // Generate the options for the dropdown list
+                    foreach($hm_prgms as $row){
+                        echo "<script>console.log('inside while')</script>";
+                        echo "<script>console.log('".$row['PRGM_ID']."')</script>";
+                        echo '<option value="' . $row['PRGM_ID'] . '">' . $row['DEPT_ID'] .' - ' .$row['PRGM_NAME'] .'</option>';
+                    }
+                    
+                    ?>
             </select>
-        </td>
-    </tr>
-        <br>
-        
-    </table>
-		<input type="submit" value="Submit" class="small_btn">
-	</form>
+            </td>
+            </tr>
 
-    <button class="small_btn" onclick="goback()">Back</button>
+            <tr>
 
-    <script>
-        function goback() {
-            window.location.href = "../admin.php";
-        }
-    </script>
+                <td><label for="preference2">Preference 2:</label></td>
+                <td><select name="preference2" id="preference2" required>
+                    <option value="">Select a Program</option>
+                <?php
 
-    <?php
-    }
-    else{
-        echo "<h1 style='text-align:center'>You aren't eligible for Honor/Minor Programmes!</h1>";
+                    // Retrieve the list of courses from the database using PDO
+                    $courses_query = "SELECT * FROM u_prgm WHERE (LOWER(prgm_name) LIKE '%honors%' AND dept_id = '$_SESSION[dept_id]') OR (LOWER(prgm_name) LIKE '%minors%' AND dept_id != '$_SESSION[dept_id]') AND offered = 1";
+                    $courses_result = $conn->query($courses_query);
 
-        //back navigation
-        ?>
+                    // Generate the options for the dropdown list
+                    while ($row = $courses_result->fetch(PDO::FETCH_ASSOC)) {
+                        echo '<option value="' . $row['PRGM_ID'] . '">' . $row['DEPT_ID'] .' - ' .$row['PRGM_NAME'] .'</option>';
+                    }
+                    
+                    ?>
+            </select>
+            </td>
+        </tr>
+        <tr>
+            <td><label for="preference3">Preference 3:</label></td>
+            <td>
+                <select name="preference3" id="preference3" required>
+                <option value="">Select a Program</option>
+                <?php
+
+                    // Retrieve the list of courses from the database using PDO
+                    $courses_query = "SELECT * FROM u_prgm WHERE (LOWER(prgm_name) LIKE '%honors%' AND dept_id = '$_SESSION[dept_id]') OR (LOWER(prgm_name) LIKE '%minors%' AND dept_id != '$_SESSION[dept_id]') AND offered = 1";
+                    $courses_result = $conn->query($courses_query);
+
+                    // Generate the options for the dropdown list
+                    while ($row = $courses_result->fetch(PDO::FETCH_ASSOC)) {
+                        echo '<option value="' . $row['PRGM_ID'] . '">' . $row['DEPT_ID'] .' - ' .$row['PRGM_NAME'] .'</option>';
+                    }
+                }
+
+                ?>
+                </select>
+            </td>
+        </tr>
+            <br>
+            
+        </table>
+            <input type="submit" value="Submit" class="small_btn">
+        </form>
+
         <button class="small_btn" onclick="goback()">Back</button>
 
         <script>
@@ -184,9 +167,24 @@
             }
         </script>
 
-    <?php
-    }
-    ?>
+        <?php
+        }
+        else{
+            echo "<h1 style='text-align:center'>You aren't eligible for Honor/Minor Programmes!</h1>";
+
+            //back navigation
+            ?>
+            <button class="small_btn" onclick="goback()">Back</button>
+
+            <script>
+                function goback() {
+                    window.location.href = "../admin.php";
+                }
+            </script>
+
+        <?php
+        }
+        ?>
     
 </body>
 </html>
